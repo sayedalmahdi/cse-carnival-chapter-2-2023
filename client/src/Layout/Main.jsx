c215a2aa98fc687351fa5d2807d22f3f91139b7f
tryit-100 <safwan.std21@gmail.com>
2023-10-14 20:59:22 +0600

    frontend init done

---
+++ client/src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2 className='text-3xl'>main</h2>
+            <button className="btn btn-neutral">Neutral</button>
+        </div>
+    );
+};
+
+export default Main;
