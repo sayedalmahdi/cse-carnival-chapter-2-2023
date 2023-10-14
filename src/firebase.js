53e4b97ba5c4d4bd64f89c6c6180f9336c25f772
Arafat Arnob <arnob001.asa@gmail.com>
2023-10-14 15:09:34 +0600

    dependency solved

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyB6RAsz3f20wEW3VcBpZMGNTIWz90-RdCc",
@@ -17,5 +18,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
+const storage = getStorage(app);
 
-export { db, auth, functions };
+export { db, auth, functions, storage };
--- src/pages/SignUpAsTourist.jsx
+++ src/pages/SignUpAsTourist.jsx
@@ -2,16 +2,14 @@
 import React from "react";
 import { useState } from "react";
 import { getDatabase, ref, set } from "firebase/database";
-import app from "../firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Signup = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const auth = getAuth(app);
 	const handleSignup = () => {
-		createUserWithEmailAndPassword(auth, email, password).then((value) =>
-			alert("success")
-		);
+		createUserWithEmailAndPassword(auth, email, password).then((value) => alert("success"));
 	};
 	const handleGoogleSignup = () => {
 		const provider = new GoogleAuthProvider();
@@ -32,21 +30,11 @@ const Signup = () => {
 	};
 
 	return (
-		<div className="flex flex-col gap-12">
+		<div className='flex flex-col gap-12'>
 			<h1>Signup</h1>
-			<div className="flex flex-col w-80 gap-5">
-				<input
-					type="text"
-					placeholder="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					placeholder="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
+			<div className='flex flex-col w-80 gap-5'>
+				<input type='text' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+				<input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
 			</div>
 			<button onClick={handleSignup}>Signup</button>
 			<button>Sign in with Google</button>
