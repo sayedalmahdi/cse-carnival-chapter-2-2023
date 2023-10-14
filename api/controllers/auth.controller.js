8b469449683d0e1c409fb417e4196350a407f38d
SI-Abid <siaabid5323@gmail.com>
2023-10-14 18:46:54 +0600

    auth-backend

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,35 @@
+import { User } from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res, next) => {
+    const { email, fullname, role, password, age} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ email, fullname, age, role, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(201).json({ message: 'New user created' });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found!'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Incorrect email or password!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token', token, {
+            httpOnly: true
+        }).status(200).json(rest);
+    } catch (err) {
+        next(err);
+    }
+}
