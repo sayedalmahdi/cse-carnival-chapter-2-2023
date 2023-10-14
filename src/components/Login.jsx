694b7da055d6c297dae2ac8900a9e65063f289ce
Ishrar2019331104 <ishrar04@student.sust.edu>
2023-10-15 02:56:33 +0600

    hmm

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,113 +1,35 @@
 import React from "react";
-import { Button, Modal, Form, Input, Checkbox, message } from "antd";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useStateValue } from "../state/StateProvider";
+import { Button, Modal } from "antd";
+import { useState } from "react";
+import { Checkbox, Form, Input } from "antd";
 
 const Login = () => {
-	const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
-	const onFinish = async (values) => {
-		try {
-			const auth = getAuth();
-			signInWithEmailAndPassword(auth, values.email, values.password);
-			message.success("Login successful!");
-			handleCancel();
-		} catch (error) {
-			console.error("Error signing in: ", error);
-			message.error("Error signing in.");
-		}
-	};
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
-	const onFinishFailed = (errorInfo) => {
-		console.log("Failed:", errorInfo);
-	};
-
-	const showModal = () => {
-		setIsModalOpen(true);
-	};
-
-	const handleOk = () => {
-		setIsModalOpen(false);
-	};
-
-	const handleCancel = () => {
-		setIsModalOpen(false);
-	};
-
-	return (
-		<>
-			<Button type='primary' onClick={showModal}>
-				Open Modal
-			</Button>
-			<Modal
-				className='custom-modal'
-				title={<h3>Login</h3>}
-				visible={isModalOpen}
-				onOk={handleOk}
-				onCancel={handleCancel}
-				footer={null}
-			>
-				<Form
-					requiredMark={false}
-					layout='vertical'
-					name='basic'
-					initialValues={{
-						remember: true,
-					}}
-					onFinish={onFinish}
-					onFinishFailed={onFinishFailed}
-					autoComplete='off'
-				>
-					<Form.Item
-						label='Email'
-						name='email'
-						rules={[
-							{
-								required: true,
-								message: "Please input your email!",
-							},
-							{
-								type: "email",
-								message: "Please enter a valid email address",
-							},
-						]}
-					>
-						<Input />
-					</Form.Item>
-
-					<Form.Item
-						label='Password'
-						name='password'
-						rules={[
-							{
-								required: true,
-								message: "Please input your password!",
-							},
-						]}
-					>
-						<Input.Password />
-					</Form.Item>
-
-					<Form.Item
-						name='remember'
-						valuePropName='checked'
-						wrapperCol={{
-							offset: 0,
-							span: 24,
-						}}
-					>
-						<Checkbox>Remember me</Checkbox>
-					</Form.Item>
-
-					<Form.Item>
-						<Button type='primary' htmlType='submit' block>
-							Submit
-						</Button>
-					</Form.Item>
-				</Form>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      
+    </>
+  );
 };
 
 export default Login;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,133 @@
-import React, { useState, useEffect } from "react";
-import { Layout } from "antd";
-
-const { Header, Content, Footer } = Layout;
+import React from "react";
+import { Typography, Input } from "antd";
+import { Button, Space } from "antd";
+const { Title, Paragraph } = Typography;
+import { Modal } from "antd";
+import { useState } from "react";
+import { Checkbox, Form } from "antd";
 
 const Home = () => {
-  const [isHeaderOpaque, setIsHeaderOpaque] = useState(false);
-
-  const handleScroll = () => {
-    if (window.scrollY > 15) {
-      setIsHeaderOpaque(true);
-    } else {
-      setIsHeaderOpaque(false);
-    }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <Layout>
-      <Header
-        className={isHeaderOpaque ? "opaque-header" : "transparent-header"}
-      ></Header>
-      <Content style={{ paddingTop: "64px", minHeight: "100vh" }}>
-        <img
-          src="https://a.cdn-hotels.com/gdcs/production129/d1735/5f9ad8cb-e195-49ef-8206-f95dba366ffd.jpg?impolicy=fcrop&w=800&h=533&q=medium"
-          alt="background"
-        />
-      </Content>
-      <Footer>Footer</Footer>
-    </Layout>
+    <>
+      <header className="header">
+        <Title
+          style={{
+            color: "#2f2e41",
+            paddingTop: "20px",
+            fontWeight: "bold",
+          }}
+          level={1}
+        >
+          GoTo
+        </Title>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Button
+                onClick={showModal}
+                style={{
+                  background: "none",
+                  color: "#2f2e41",
+                  border: "1px solid #2f2e41",
+                }}
+                className="login-button"
+              >
+                Login
+              </Button>
+            </li>
+            <li className="nav-item">
+              <Button style={{}} className="rounded-button">
+                Sign Up
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="tagline"></div>
+      <Modal
+        className="custom-modal" // Add this class for custom styling
+        title={<h3>Login</h3>}
+        visible={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form
+          requiredMark={false}
+          layout="vertical"
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 0,
+              span: 24,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      ;
+    </>
   );
 };
 
