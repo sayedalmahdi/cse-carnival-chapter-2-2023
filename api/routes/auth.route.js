d7a1c1b593af11684ec663a7d833dbc068519830
SI-Abid <siaabid5323@gmail.com>
2023-10-15 13:01:49 +0600

    backend updated

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import { signin, signup, verify } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 
 router.post('/signin', signin);
-
-router.get('/verify:token', verify);
 
 export default router;
